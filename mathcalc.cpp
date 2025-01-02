db75dcd9cf9fa1984203f9342e990f562034b3d5
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-01-02 17:18:40 +0200

    baseplan

--- mathcalc.cpp
+++ mathcalc.cpp
@@ -1,8 +1,4 @@
 #include "mathcalc.h"
-
-
-
-
 /*
 operator order:
 
@@ -20,16 +16,37 @@ pop 2 elems from stack1
 pop 1 elem from stack2
 push 1 elem in stack1 
 while (stack2!=empty)
-
 */
 
+// op == operator, od == order
+std::unordered_map<const char, const int8_t> opod{
+    {'(', 0},
+    {')', 0},
+    {'*', 1},
+    {'/', 1},
+    // idk inca pt modulo ce sa fac da om vedea
+    {'+', 2},
+    {'-', 2}
+};
 
 
 std::string solve(std::string operand){
-    std::string solution="";
+    std::string solution="", tempNumb="";
     
-    // solve what is in string
-    // check upper comment for help
-
-    return solution+"Hello World!";
+    std::stack<number> nums;
+    std::stack<char> oper;
+    for (auto chr:operand){
+        if (isdigit(chr)){
+            tempNumb.push_back(chr);
+        } else {
+            nums.push(number(tempNumb));
+            tempNumb.clear();
+            if (opod[oper.top()]>=opod[chr]){
+                oper.push(chr);
+            } else {
+                // code to calculate the stuff in nums and oper
+            }
+        }
+    }
+    return solution;
 }
--- mathcalc.h
+++ mathcalc.h
@@ -2,8 +2,12 @@
 
 // libs
 #include <string>
+#include <stack>
+#include <unordered_map>
+
 
 // files
 #include "mathcalc.cpp"
+#include "number.h"
 
 std::string solve(std::string);
