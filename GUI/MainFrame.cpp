9e9ac2b8f4942d464c5ac2a2a9cad2db3f7ba01a
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-01-04 17:22:51 +0200

    more controls

--- GUI/MainFrame.cpp
+++ GUI/MainFrame.cpp
@@ -10,4 +10,7 @@ MainFrame::MainFrame(const wxString& title):
 	wxPanel* panel = new wxPanel(this);
 	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35));
 	wxCheckBox* chbox = new wxCheckBox(panel, wxID_ANY, "CheckBox", wxPoint(550, 50));
+	wxStaticText* stText = new wxStaticText(panel, wxID_ANY, "Static Text", wxPoint(120, 150)); // not changeble by the user
+	wxTextCtrl* txtCtr = new wxTextCtrl(panel, wxID_ANY, "Text Control", wxPoint(500, 145)); // can be changed by user
+	wxSlider* slider = new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(100, 250), wxSize(200, -1));
 }
