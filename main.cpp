a13be3ebd595bc0f1f3605a51db6e5268fc564e2
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-01-04 02:08:21 +0200

    i can no longer use vscode (for now, will fix soon)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-
+/*
 #include "mathcalc.h"
 
 std::string input_reader() {
@@ -8,12 +8,35 @@ std::string input_reader() {
     std::getline(std::cin, s);
     return s;
 }
-int main() {
+void backend(){
     try {
         std::cout << solve(input_reader());
     }
     catch (char const* error) {
         std::cout << error;
     }
+}*/
+
+
+#include <wx/wx.h>
+
+class App : public wxApp {
+public:
+    bool OnInit() {
+        wxFrame* window = new wxFrame(NULL, wxID_ANY, "GUI Test", wxDefaultPosition, wxSize(600, 400));
+        wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+        wxStaticText* text = new wxStaticText(window, wxID_ANY, "Well Done!\nEverything seems to be working",
+            wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
+        text->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+        sizer->Add(text, 1, wxALIGN_CENTER);
+        window->SetSizer(sizer);
+        window->Show();
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(App);
+
+/*int main() {
     return 0;
-}
+}*/
