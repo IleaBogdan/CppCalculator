ac89293793f7c04168e9b19efb76d0f0331a1cb9
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-01-04 17:15:36 +0200

    button and checkbox

--- GUI/MainFrame.cpp
+++ GUI/MainFrame.cpp
@@ -2,5 +2,12 @@
 
 MainFrame::MainFrame(const wxString& title): 
 	wxFrame(nullptr, wxID_ANY, title){
-	// empty for now
+	
+	// controls:
+
+	// we need to add all controls in a panel
+	// if not we will have all of them spreading on the screen randomly
+	wxPanel* panel = new wxPanel(this);
+	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35));
+	wxCheckBox* chbox = new wxCheckBox(panel, wxID_ANY, "CheckBox", wxPoint(550, 50));
 }
