04d3ddcb8143d9a4654ebe41222a14bd9ba8cf68
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-01-05 01:03:36 +0200

    random button from god knows were added (please help)

--- GUI/MainFrame.cpp
+++ GUI/MainFrame.cpp
@@ -1,5 +1,7 @@
 #include "MainFrame.h"
 #include <vector>
+#include <unordered_map>
+#include <string>
 
 MainFrame::MainFrame(const wxString& title): 
 	wxFrame(nullptr, wxID_ANY, title){
@@ -14,20 +16,22 @@ MainFrame::MainFrame(const wxString& title):
 	// init numpad
 	// maybe will make it another function
 	std::pair<int, int> cpl{30, 260}; // core point location
-	std::vector<wxPoint> numpad_points = { 
-		{cpl.first+25, cpl.second+75}, // 0 
-		{cpl.first, cpl.second}, // 1
-		{cpl.first+75, cpl.second}, // 2
-		{cpl.first+2*75, cpl.second}, // 3
-		{cpl.first, cpl.second+25}, // 4
-		{cpl.first+75, cpl.second+25}, // 5
-		{cpl.first+2*75, cpl.second+25}, // 6
-		{cpl.first, cpl.second+2*25}, // 7
-		{cpl.first+75, cpl.second+2*25}, // 8
-		{cpl.first+2*75, cpl.second+2*25}  // 9
+	std::unordered_map<char, wxPoint> numpad_points{
+		{'0', {cpl.first + 25, cpl.second + 75}}, // 0 
+		{'1', {cpl.first, cpl.second} }, // 1
+		{'2', {cpl.first + 75, cpl.second} }, // 2
+		{'3', {cpl.first + 2 * 75, cpl.second} }, // 3
+		{'4', {cpl.first, cpl.second + 25}}, // 4
+		{'5', {cpl.first + 75, cpl.second + 25}}, // 5
+		{'6', {cpl.first + 2 * 75, cpl.second + 25}}, // 6
+		{'7', {cpl.first, cpl.second + 2 * 25}}, // 7
+		{'8', {cpl.first + 75, cpl.second + 2 * 25}}, // 8
+		{'9', {cpl.first + 2 * 75, cpl.second + 2 * 25} } // 9
 	};
 	std::vector<wxButton*> numpad;
-	for (int i:{1,4,7,2,5,8,0,3,6,9}) {
-		numpad.push_back(new wxButton(panel, wxID_ANY, wxString::Format(wxT("%i"), i), numpad_points[i]));
+	for (char i:"1472580369") {
+		numpad.push_back(new wxButton(panel, wxID_ANY, wxString(std::string{i}), numpad_points[i]));
 	}
+	numpad.pop_back();
+	wxTextCtrl* conosole = new wxTextCtrl(panel, wxID_ANY, "wtf?");
 }
