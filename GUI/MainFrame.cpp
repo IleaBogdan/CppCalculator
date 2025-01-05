547d0f9df4dc34256a15939379e9079d88480316
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-01-06 00:43:57 +0200

    = button added

--- GUI/MainFrame.cpp
+++ GUI/MainFrame.cpp
@@ -1,6 +1,7 @@
 #include "MainFrame.h"
 #include <vector>
 #include <string>
+#include <iostream>
 
 MainFrame::MainFrame(const wxString& title): 
 	wxFrame(nullptr, wxID_ANY, title){
@@ -14,10 +15,17 @@ MainFrame::MainFrame(const wxString& title):
 	);
 	std::vector<wxButton*> numpad=init_numpad(panel);
 	for (auto& numpadelem : numpad) {
+		//std::cout << numpadelem->GetId() << std::endl;
 		numpadelem->Bind(wxEVT_BUTTON, &MainFrame::OnButtonClick, this);
 	}
 }
 
 void MainFrame::OnButtonClick(wxCommandEvent& e) {
-	wxLogStatus(wxString(std::to_string(e.GetId()-2)));
+	int localid = e.GetId();
+	char localid_value = char(localid);
+	wxLogStatus(wxString{localid_value});
+	if (localid_value == '=') {
+		std::cout << "TO DO: call the backend here\n";
+	}
+	
 }
--- GUI/Numpad.cpp
+++ GUI/Numpad.cpp
@@ -18,14 +18,15 @@ std::vector<wxButton*> init_numpad(wxWindow* parrent) {
 		{'6', {cpl.first + 2 * 75, cpl.second + 25}},
 		{'7', {cpl.first, cpl.second + 2 * 25}},
 		{'8', {cpl.first + 75, cpl.second + 2 * 25}},
-		{'9', {cpl.first + 2 * 75, cpl.second + 2 * 25} }
+		{'9', {cpl.first + 2 * 75, cpl.second + 2 * 25} },
+		{'=', {cpl.first+25*4, cpl.second+75}}
 	};
 	
 	std::vector<wxButton*> numpad;
-	for (char i : std::string("1234567890")) {
+	for (char i : std::string("1234567890=")) {
 		numpad.push_back(new wxButton(parrent,
 			// setting each button a costum ID
-			static_cast<Numpad_IDs>(i-'0'+2),
+			int(i),
 			wxString(std::string{ i }), numpad_points[i]));
 	}
 	return numpad;
